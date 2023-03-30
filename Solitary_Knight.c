ca139c620cb9ffea64c37eed8adfbceed85b2df6
Zaldef <78030935+Zaldef@users.noreply.github.com>
2023-03-30 12:11:21 -0300

    0.3 - Transição de mapa
    Quando bate nas extremidades direita e inferior acontece um bug

--- Solitary_Knight.c
+++ Solitary_Knight.c
@@ -6,9 +6,10 @@
 #include <allegro5/allegro_ttf.h> // Renderizar fontes
 #include <allegro5/keyboard.h> // Teclado
 #include <stdio.h>
-int TELA_ALTURA = 480;
-int TELA_LARGURA = 480;
-int mapa = 1;
+int TELA_ALTURA = 640;
+int TELA_LARGURA = 640;
+int mapa_X = 2560;
+int mapa_Y = 2560;
 
 int main() {
 
@@ -18,12 +19,11 @@ int main() {
   al_install_keyboard();
 
   ALLEGRO_DISPLAY *display = al_create_display(TELA_LARGURA, TELA_ALTURA);
-  al_set_window_position(display, 200, 200);
+  al_set_window_position(display, 50, 50);
   ALLEGRO_FONT *font = al_create_builtin_font();
   ALLEGRO_TIMER *timer = al_create_timer(1.0 / 30.0);
   ALLEGRO_BITMAP *knight = al_load_bitmap("./images/Chars/actor9.png");
-  ALLEGRO_BITMAP *villa = al_load_bitmap("./images/Mapas/villa2.png");
-  ALLEGRO_BITMAP *villa2 = al_load_bitmap("./images/Mapas/villa3.1.png");
+  ALLEGRO_BITMAP *mapa = al_load_bitmap("./images/Mapas/mapa1.png");
   
 
   ALLEGRO_EVENT_QUEUE *event_queue = al_create_event_queue();
@@ -31,8 +31,8 @@ int main() {
   al_register_event_source(event_queue, al_get_keyboard_event_source());
   al_register_event_source(event_queue, al_get_timer_event_source(timer));
   al_start_timer(timer);
-
-  int xknight = 0, yknight = 0;
+  
+  int xknight = 0, yknight = 0, xmapa = 0, ymapa = 0;
   float frame = 0.f;
   int current_frame_y = 0;
 
@@ -44,18 +44,18 @@ int main() {
 
     if (event.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
       break;
-    }else if( event.keyboard.keycode == ALLEGRO_KEY_S ){
+    }else if( event.keyboard.keycode == ALLEGRO_KEY_S){
       current_frame_y = 32 * 0;
-      yknight += 1;
-    }else if( event.keyboard.keycode == ALLEGRO_KEY_A ){
+      yknight += 32;
+    }else if( event.keyboard.keycode == ALLEGRO_KEY_A){
       current_frame_y = 32 * 1;
-      xknight -= 1;
-    }else if( event.keyboard.keycode == ALLEGRO_KEY_D ){
+      xknight -= 32;
+    }else if( event.keyboard.keycode == ALLEGRO_KEY_D){
       current_frame_y = 32 * 2;
-       xknight += 1;
-    }else if( event.keyboard.keycode == ALLEGRO_KEY_W ){
+      xknight += 32;
+    }else if( event.keyboard.keycode == ALLEGRO_KEY_W){
       current_frame_y = 32 * 3;
-      yknight -= 1;
+      yknight -= 32;
     }
 
     frame += 0.3f;
@@ -66,46 +66,44 @@ int main() {
     ////////////////////////CONTROLES//////////////////////////////////////////////
 
     if (yknight < 0) {
-      yknight += 1;
+      ymapa += 640;
+      yknight = 640;
     }
     if (xknight < 0) {
-      yknight += 1;
+      xmapa += 640;
+      xknight = 640;
     }
-    if (yknight > TELA_ALTURA-32) {
-      yknight -= 1;
+    if (yknight > TELA_ALTURA-32 && ymapa != -1920){
+      ymapa -= 640;
+      yknight = 0;
     }
-    if (xknight > TELA_LARGURA-32) {
-      xknight -= 1;
+    if (xknight > TELA_LARGURA-32 && xmapa != -1920){
+      xmapa -= 640;
+      xknight = 0;
     }
     ////////////////////////LIMITES DA TELA//////////////////////////////////////
 
-    if (mapa = 1) {
-      //al_clear_to_color(al_map_rgb(255, 255, 255));
-      al_draw_bitmap(villa2, 0, 0, 0);
+      al_draw_bitmap(mapa, xmapa, ymapa, 0);
       al_draw_bitmap_region(knight, 32 * (int)frame,current_frame_y,32,32,xknight,yknight,0);
-      if (yknight < 440 && yknight > 315 && xknight > 10 && xknight < 325){
-        yknight += 1;
-      }
-      if (yknight < 190 && yknight > 10 && xknight < 355){
-        xknight += 1;
-      }
-      if (xknight < 10 && yknight < 0 && yknight > 60){
-        xknight -= 1;
-      }
+      
       ///COLISAO///
-    }
+ 
     ////////////////////////Definições de mapa//////////////////////////////////////////////
 
-    char xbposicao[50];
-    char ybposicao[50];
-    char nmapa[50];
-    sprintf(nmapa, "mapa: é %d", mapa);
-    sprintf(xbposicao, "xknight: é %d", xknight);
-    sprintf(ybposicao, "yknight: é %d", yknight);
+    char xkposicao[50];
+    char ykposicao[50];
+    char xmposicao[50];
+    char ymposicao[50];
 
-    al_draw_text(font, al_map_rgb(0, 0, 0), 0, 10, 0, xbposicao);
-    al_draw_text(font, al_map_rgb(0, 0, 0), 0, 20, 0, ybposicao);
-    al_draw_text(font, al_map_rgb(0, 0, 0), 0, 30, 0, nmapa);
+    sprintf(xkposicao, "xknight: é %d", xknight);
+    sprintf(ykposicao, "yknight: é %d", yknight);
+    sprintf(xmposicao, "xmapa: é %d", xmapa);
+    sprintf(ymposicao, "ymapa: é %d", ymapa);
+
+    al_draw_text(font, al_map_rgb(255, 255, 255), 0, 10, 0, xkposicao);
+    al_draw_text(font, al_map_rgb(255, 255, 255), 0, 20, 0, ykposicao);
+    al_draw_text(font, al_map_rgb(255, 255, 255), 0, 30, 0, xmposicao);
+    al_draw_text(font, al_map_rgb(255, 255, 255), 0, 40, 0, ymposicao);
     al_flip_display();
     ////////////////////////EXIBIR CORDENADAS//////////////////////////////////////////////
   }
@@ -113,7 +111,7 @@ int main() {
   al_destroy_display(display);
   al_destroy_event_queue(event_queue);
   al_destroy_bitmap(knight);
-  al_destroy_bitmap(villa);
+  al_destroy_bitmap(mapa);
   //Encerrando
   return 0;
 }
